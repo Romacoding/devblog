9a90ecf7e82312854e67b4c24b1f327af282aa09
Roman Ostash <34969764+Romacoding@users.noreply.github.com>
2018-08-14 12:54:04 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'Roman Ostash\'s blog', // Required
   author: 'Roman Ostash', // Required
   tagline: 'Web Developer',
-  primaryColor: '#8ee8d8', // Required
+  primaryColor: '#4dc0b9', // Required
   showHeaderImage: true,
   showShareButtons: true,
   postsPerPage: 5, // Required
